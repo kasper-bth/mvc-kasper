e8dec7294929c797eadc2d5a746ceb8596f1278f
kasper-bth <kahg24@student.bth.se>
2025-04-17 16:42:40 +0200

    Fixed documentation and landing page

---
+++ src/Controller/CardGameController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Card\DeckOfCards;
+use App\Card\CardHand;
+
+class CardGameController extends AbstractController
+{
+    #[Route("/game", name: "game_home")]
+    public function home(): Response
+    {
+        return $this->render('game/home.html.twig');
+    }
+
+    #[Route("/game/doc", name: "game_doc")]
+    public function doc(): Response
+    {
+        return $this->render('game/doc.html.twig');
+    }
+}
