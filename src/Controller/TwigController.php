d6833c2654e0a3b6bfdf256c3bd6588b0720a736
kasper-bth <kahg24@student.bth.se>
2025-05-15 14:05:29 +0200

    Start of code analysis

--- src/Controller/TwigController.php
+++ src/Controller/TwigController.php
@@ -43,4 +43,10 @@ class TwigController extends AbstractController
     {
         return $this->render('api.html.twig');
     }
+
+    #[Route("/metrics", name: "metrics")]
+    public function metrics(): Response
+    {
+        return $this->render('metrics.html.twig');
+    }
 }
