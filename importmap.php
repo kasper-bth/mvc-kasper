88dae30653482d713353b64e540dc39522f88b26
kasper-bth <kahg24@student.bth.se>
2025-05-23 14:56:13 +0200

    Start of project

--- importmap.php
+++ importmap.php
@@ -16,6 +16,10 @@ return [
         'path' => './assets/app.js',
         'entrypoint' => true,
     ],
+    'proj' => [
+        'path' => './assets/proj.js',
+        'entrypoint' => true,
+    ],
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
